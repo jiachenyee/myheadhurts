0d03997d54e7a3eb61bb6711ef848388b898ec0c
Jia Chen <github@jiachen.app>
2024-12-18 11:13:31 +0800

    Initial Commit

---
+++ myheadhurts/myheadhurtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  myheadhurtsApp.swift
+//  myheadhurts
+//
+//  Created by Jia Chen Yee on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct myheadhurtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
